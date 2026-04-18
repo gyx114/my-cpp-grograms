b2802a7e0588b2fbfd31d1a420bda1a47e743784
gyx114 <guanyuxuan01@outlook.com>
2026-04-18 23:31:38 +0800

    练

--- leetcode/28.cpp
+++ leetcode/28.cpp
@@ -5,14 +5,15 @@ class Solution
 public:
     int strStr(string haystack, string needle)
     {
-        const char *p = haystack.c_str();
-        const char *q = needle.c_str();
-        const char *dest = strstr(p, q);
-        if (dest)
-        {
+        // const char *p = haystack.c_str();
+        // const char *q = needle.c_str();
+        // const char *dest = strstr(p, q);
+        // if (dest)
+        // {
 
-            return dest - haystack.c_str();
-        }
-        return -1;
+        //     return dest - haystack.c_str();
+        // }
+        // return -1;
+        return haystack.find(needle);
     }
 };
