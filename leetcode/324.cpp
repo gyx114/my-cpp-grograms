7dd44b65a7dd87fcbaf3338d3313aab596c1f644
gyx114 <guanyuxuan01@outlook.com>
2026-06-27 01:28:56 +0800

    修改

--- leetcode/324.cpp
+++ leetcode/324.cpp
@@ -5,11 +5,18 @@ class Solution
 public:
     void wiggleSort(vector<int> &nums)
     {
-        sort(nums.begin(), nums.end());
         int n = nums.size();
-        for (int i = 1; i < (n + 1) / 2; i += 2)
+        vector<int> arr(n);
+        sort(nums.begin(), nums.end());
+        int x = (n + 1) / 2;
+        for (int i = 0, j = x - 1, k = n - 1; i < n; i += 2, j--, k--)
         {
-            swap(nums[i], nums[(n + 1) / 2 + i]);
+            arr[i] = nums[j];
+            if (i + 1 < n)
+            {
+                arr[i + 1] = nums[k];
+            }
         }
+        nums.swap(arr);
     }
 };
